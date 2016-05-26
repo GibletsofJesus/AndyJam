5f67e8ea91bdbe0eb305985fe882f8cfdc53bfb8
Craig <ctinney94@gmail.com>
2016-05-26 16:05:35 +0100

    BARREL ROLL BITCHES

--- Unity/Assets/scripts/playerMovement.cs
+++ Unity/Assets/scripts/playerMovement.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class playerMovement : Actor {
 
-    public GameObject parent;
-
     Rigidbody2D rig;
     public float moveSpeed;
 
@@ -25,7 +23,7 @@ public class playerMovement : Actor {
         float angle=0;
 
         //If difference in y rotation is voer 60 degrees 
-        if (transform.rotation.eulerAngles.y < 40 && rotLerp.y == -45)
+        /*if (transform.rotation.eulerAngles.y < 40 && rotLerp.y == -45)
         {
             StartCoroutine(doABarrrelRoll(1));
         }
@@ -33,7 +31,7 @@ public class playerMovement : Actor {
         {
             StartCoroutine(doABarrrelRoll(-1));
         }
-
+        */
         if (!rolling)
             angle = Mathf.LerpAngle(transform.rotation.eulerAngles.y, rotLerp.y, Time.deltaTime * 5);
 
@@ -43,48 +41,53 @@ public class playerMovement : Actor {
     bool rolling;
     IEnumerator doABarrrelRoll(int dir)
     {
+        Debug.Log("rolling");
         rolling = true;
-        Vector3 initialRot = parent.transform.rotation.eulerAngles;
+        Vector3 initialRot = transform.rotation.eulerAngles;
         Vector3 goalRot = rotLerp;
         float t=0;
-        if (dir > 0)
-        {
-            goalRot.y += 360;
-        }
-        else
-        {
-            goalRot.y -= 360;
-        }
 
         Vector3 currentRot = initialRot;
 
         while (t < .5f)
         {
-            currentRot.y = Mathf.Lerp(initialRot.y, goalRot.y, t / .5f);
-            transform.rotation= Quaternion.Euler(currentRot);
+            currentRot.y = Mathf.Lerp(initialRot.y, rotLerp.y + ((dir > 0) ? 360 : -90), t / .5f);
+            transform.rotation = Quaternion.Euler(currentRot);
             t += Time.deltaTime;
             yield return null;
         }
 
         rolling = false;
-        //parent.transform.Rotate(Vector3.left);
+        Debug.Log("Stop rolling");
     }
 
     void inputThings()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
+            if (!rolling)
+            {
+                if (transform.rotation.eulerAngles.y > 314 && transform.rotation.eulerAngles.y < 317)
+                    StartCoroutine(doABarrrelRoll(-1));
+
+                rotLerp = new Vector3(0, 45, 0);
+            }
             rig.AddForce(-Vector2.right * moveSpeed, ForceMode2D.Impulse);
-            rotLerp = new Vector3(0, 45,0);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
+            if (!rolling)
+            {
+                if (transform.rotation.eulerAngles.y > 42 && transform.rotation.eulerAngles.y < 45)
+                    StartCoroutine(doABarrrelRoll(1));
+                rotLerp = new Vector3(0, -45, 0);
+            }
+            
             rig.AddForce(Vector2.right * moveSpeed, ForceMode2D.Impulse);
-            rotLerp = new Vector3(0,-45,0);
         }
         else
         {
-            rotLerp = Vector3.zero ;
+            rotLerp = Vector3.zero;
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
