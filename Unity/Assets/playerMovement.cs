4344e76b5429c4d9bdf5b34e7b09753aaa5d8010
Craig <ctinney94@gmail.com>
2016-05-26 12:46:08 +0100

    bsajhgdkjsahgdkjsahkdsa

---
+++ Unity/Assets/playerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+    Rigidbody2D rig;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rig = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        inputThings();
+	}
+
+    void inputThings()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rig.AddForce(
+            }
+
+    }
+}
