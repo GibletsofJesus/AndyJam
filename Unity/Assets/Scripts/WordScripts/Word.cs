225b15b1263d952dec6ad63716a3fce91c2afa47
Shaun Landy <Shaun2.Landy@live.uwe.ac.uk>
2016-05-26 14:47:40 +0100

    Word Scripts

---
+++ Unity/Assets/Scripts/WordScripts/Word.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Word : MonoBehaviour 
+{
+	private string thisWord = string.Empty;
+	public string word {get {return thisWord;} set{ thisWord = value;}}
+
+	[Range(0.0f, 300.0f)]public float wordCooldown = 10.0f;
+	private float currentCooldown;
+
+	private delegate void BehaviorDelegate();
+	private BehaviorDelegate behavior = null;
+
+	private void Start()
+	{
+		currentCooldown = wordCooldown;
+	}
+
+	private void Update()
+	{
+		currentCooldown = Mathf.Min(currentCooldown + Time.deltaTime, wordCooldown);
+	}
+
+	public bool Match(string _word)
+	{
+		if(thisWord == word)
+		{
+			ActivateBehavior();
+			return true;
+		}
+		return false;
+	}
+
+	public void ActivateBehavior()
+	{
+		//Activate behavior
+		if(wordCooldown == currentCooldown)
+		{
+			currentCooldown = 0.0f;
+			behavior();
+		}
+		//Behavior on cooldown, do something to show this (flicker red?)
+		else
+		{
+
+		}
+	}
+
+	public abstract void Behavior();
+}
---
+++ Unity/Assets/Scripts/WordScripts/WordBuffer.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WordBuffer : MonoBehaviour 
+{
+	private static WordBuffer staticInstance = null;
+	public static WordBuffer instance {get {return instance;} set{}}
+
+	private string currentWord;
+	private const int MaxCharacters = 20;
+
+	private List<Word> words = new List<Word>();
+
+	void Start()
+	{
+		staticInstance = this;
+	}
+
+	void Update()
+	{
+		foreach (char _c in Input.inputString) 
+		{
+			//If backspace then clear the word
+			if(_c == '\b')
+			{
+				currentWord = string.Empty;
+			}
+			//Else other empty space value then do nothing
+			else if((int)_c < 33 || (int)_c > 126)
+			{
+			}
+			//Add to word string
+			else
+			{
+				if(currentWord.Length <= MaxCharacters)
+				{
+					currentWord += _c;
+					//Test to see if any words match
+					foreach (Word _w in words) 
+					{
+						if(_w.Match(currentWord))
+						{
+							currentWord = string.Empty;
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+}
