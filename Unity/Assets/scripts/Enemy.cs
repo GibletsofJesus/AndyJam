3db4eb95ee2bc97fa6d33c7e4aac3d7ef97be065
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-05-26 13:47:29 +0100

    enemies-ish

---
+++ Unity/Assets/scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour 
+{
+    RaycastHit2D hit;
+    private float health;
+    Projectile projectile;
+    float speed;
+    void Awake()
+    {
+        projectile = GetComponent<Projectile>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+    public void SetEnemy(float _health,float _speed)
+    {
+        speed = _speed;
+        health = _health;
+    }
+    void Shoot()
+    {
+       hit = Physics2D.Raycast(transform.position + transform.up, transform.up, 10);
+       if (hit.rigidbody.gameObject.tag == "Player")
+       {
+           projectile.Fire(transform.up);
+       }
+
+    }
+    void Movement()
+    {
+        Vector2 movement = transform.up * (speed * Time.deltaTime);
+        transform.Translate(movement, Space.World);
+    }
+
+}
---
+++ Unity/Assets/scripts/Projectile.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+
+    float damage;
+    float speed;
+    float aliveTime;
+    float currentAliveTime;
+
+    // Use this for initialization
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Fire(Vector2 _direction)
+    {
+        transform.Translate(_direction);
+    }
+    public void WeaponVariables(float _damage, float _speed,float _aliveTime)
+    {
+        damage = _damage;
+        speed = _speed;
+        aliveTime = _aliveTime;
+        currentAliveTime = aliveTime;
+    }
+    void Alive()
+    {
+        if (currentAliveTime <=0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.GetComponent<playerMovement>())
+        {
+            //do a thing
+        }
+         gameObject.SetActive(false);
+       
+    }
+}
