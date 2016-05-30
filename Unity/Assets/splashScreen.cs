800afe5554047e641d2454588644f3af79d4832f
Craig <ctinney94@gmail.com>
2016-05-30 17:47:33 +0100

    Splash screen! :D

---
+++ Unity/Assets/splashScreen.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class splashScreen : MonoBehaviour {
+
+    public Animator anim;
+    public bool allowStart;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Input.GetKeyDown(KeyCode.Return) && allowStart)
+        {
+            anim.StopPlayback();
+            anim.Play("splash_out");
+            allowStart = false;
+        }
+	}
+
+    public void letPlayerStart()
+    {
+        allowStart = true;
+    }
+
+    public void loadLevel()
+    {
+        Application.LoadLevel(1);
+    }
+}
