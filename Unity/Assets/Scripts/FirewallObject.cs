5a4087a6b6d8327e21afa21a6013d45440304880
LaserAce <shaunlandy11@hotmail.com>
2016-05-30 22:36:58 +0100

    Firewall

---
+++ Unity/Assets/Scripts/FirewallObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Yes, this may seem like a waste, if we used interfaces this would have been nicer
+public class FirewallObject : Actor
+{
+    //Gradually rotate firewall
+    protected override void Update()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+    //Do not perform y rotation of firewall
+    private void LateUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, transform.eulerAngles.z);
+    }
+}
--- Unity/Assets/scripts/WordScripts/Words/Firewall.cs
+++ Unity/Assets/scripts/WordScripts/Words/Firewall.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class Firewall : AbilityWord
 {
-	[SerializeField] private float wallHealth;
+    [SerializeField] private GameObject[] firewalls = null;
 
-	protected override void Start ()
+    protected override void Start ()
 	{
 		wordTiers = new string[] {"firewall.exe", "firewall.exe", "firewall.exe"};
 		base.Start ();
@@ -14,23 +14,23 @@ public class Firewall : AbilityWord
 	protected override void TriggerBehavior ()
 	{
 		base.TriggerBehavior ();
+        for(int i = 0; i < currentTier + 1; ++i)
+        {
+            firewalls[i].SetActive(true);
+        }
 	}
 	
 	protected override void Behavior ()
 	{
-		switch (currentTier) 
-		{
-		case 0:
-			break;
-		case 1:
-			break;
-		case 2:
-			break;
-		}
-	}
+        //If last firewall broken
+        if (!firewalls[0].activeSelf)
+        {
+            EndBehavior();
+        }
+    }
 	
 	protected override void EndBehavior()
 	{
 		base.EndBehavior ();
-	}
+    }
 }
