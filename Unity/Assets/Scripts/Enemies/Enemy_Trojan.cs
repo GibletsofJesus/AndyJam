2d4fb750eabf8b91251d283b47dde6bfc2261a69
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-06-01 13:09:57 +0100

    splitting trojans

--- Unity/Assets/Scripts/Enemies/Enemy_Trojan.cs
+++ Unity/Assets/Scripts/Enemies/Enemy_Trojan.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Enemy_Trojan : Enemy
 {
+    bool split = true;
     protected override void Awake()
     {
         base.Awake();
@@ -32,6 +33,27 @@ public class Enemy_Trojan : Enemy
             shootDir.x = Mathf.Clamp(shootDir.x, -5, 0);
 
         return base.Shoot(_projData, shootDir, _shootTransform);
+    }
+    protected override void Death()
+    {
+       if (split)
+       {
+           for (int i = 0; i < 2; i++)
+           {
+               Enemy e = EnemyManager.instance.EnemyPooling(this);
+               e.GetComponent<Enemy_Trojan>().SetSplit(false);
+               e.transform.position = new Vector2(e.transform.position.x + (i*1.5f),e.transform.position.y);
+               e.transform.localScale = new Vector3(0.7f,0.7f,0.7f);
+           }
+
+       }
+       base.Death();
+
+
+    }
+    void SetSplit(bool b)
+    {
+        split = b;
     }
 
 }
