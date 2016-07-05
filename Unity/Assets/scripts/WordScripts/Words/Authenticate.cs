2aeb0e6e9e5e46ce64ff2dc347a2ab12d7086096
Shaun Landy <Shaun2.Landy@live.uwe.ac.uk>
2016-07-05 16:35:50 +0100

    Laser

--- Unity/Assets/scripts/WordScripts/Words/Authenticate.cs
+++ Unity/Assets/scripts/WordScripts/Words/Authenticate.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Authenticate : AbilityWord
 {
+    [SerializeField] private LaserModule laser = null;
+
 	protected override void Start ()
 	{
 		wordTiers = new string[] {"authenticate.exe", "authenticate.exe", "authenticate.exe"};
@@ -12,19 +14,15 @@ public class Authenticate : AbilityWord
 	protected override void TriggerBehavior ()
 	{
 		base.TriggerBehavior ();
+        laser.FireLaser();
 	}
 	
 	protected override void Behavior ()
 	{
-		switch (currentTier) 
-		{
-		case 0:
-			break;
-		case 1:
-			break;
-		case 2:
-			break;
-		}
+		if(!laser.IsLaserFiring())
+        {
+            EndBehavior();
+        }
 	}
 	
 	protected override void EndBehavior()
