d33ed402eb032a85804bac5e430b97574f90d137
dialcforchris <christopher2.mugridge@live.uwe.ac.uk>
2016-07-05 17:55:30 +0100

    main menu now in

--- Unity/Assets/Scripts/splashScreen.cs
+++ Unity/Assets/Scripts/splashScreen.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
-public class splashScreen : MonoBehaviour {
+public class splashScreen : MonoBehaviour
+{
 
     public Animator anim;
     public SpriteRenderer ship;
@@ -9,16 +13,37 @@ public class splashScreen : MonoBehaviour {
     public bool allowStart;
     public AudioSource swapShipSound;
     public AudioClip[] swapSounds;
-	// Update is called once per frame
+    [SerializeField] private Text[] options;
+    [SerializeField] private GameObject[] titles;
+    [SerializeField] Text[] scores;
+    [SerializeField] Text[] names;
+    [SerializeField] Text[] rank;
+    [SerializeField]
+    Canvas lBoard;
+    bool once = false;
+    private int menuSelect = 0;
+    private float menuCool = 0.5f;
+    private float maxMenuCool = 0.5f;
+    private float buttonCool = 0.5f;
+    private float maxButtonCool = 0.5f;
+    bool leaderBool = false;
+    // Update is called once per frame
+    void Awake()
+    {
+       
+    }
 	void Update ()
     {
-	    if (Input.GetKeyDown(KeyCode.Return) && allowStart)
+       
+        if (allowStart)
         {
-            anim.StopPlayback();
-            anim.Play("splash_out");
-            allowStart = false;
+            OptionSelect();
+            ChangeMenuColour();
         }
-        if (Input.GetKeyDown(KeyCode.C))
+        MenuCooldown();
+        ButtonCoolDown();
+        DisplayLeaderBoard();
+        if (Input.GetButton("Fire2")&&ButtonCoolDown())
         {
             swapShipSound.PlayOneShot(swapSounds[(ship.sprite == ShipA) ? 0 : 1]);
             if (ship.sprite == ShipA)
@@ -28,6 +53,39 @@ public class splashScreen : MonoBehaviour {
             else
             {
                 ship.sprite = ShipA;
+            }
+            buttonCool = 0;
+        }
+        if (Input.GetButton("Fire1") && allowStart && ButtonCoolDown())
+        {
+            switch (menuSelect)
+            {
+                case 0:
+                    {
+                        anim.StopPlayback();
+                        GreenShip.instance.ship = ship.sprite;
+                        anim.Play("splash_out");
+                        foreach (Text t in options)
+                        {
+                            t.CrossFadeColor(new Color(0, 0, 0, 0), 0.3f, false, true);
+                        }
+                        allowStart = false;
+
+                        break;
+                    }
+                case 1:
+                    {
+                        leaderBool = !leaderBool;
+                        ShowLeaderBoard(leaderBool);
+                        buttonCool = 0;
+                        break;
+                    }
+                case 2:
+                    {
+                        if (Input.GetButton("Fire1") && allowStart && MenuCooldown())
+                            Application.Quit();
+                        break;
+                    }
             }
         }
 	}
@@ -39,6 +97,114 @@ public class splashScreen : MonoBehaviour {
 
     public void loadLevel()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1);
+    }
+
+    void OptionSelect()
+    {
+        if (Input.GetAxis("Vertical") != 0 && MenuCooldown())
+        {
+            if (Input.GetAxis("Vertical")<0)
+            {
+                menuSelect++;
+                if (menuSelect>options.Length-1)
+                {
+                    menuSelect = 0;
+                }
+            }
+            if (Input.GetAxis("Vertical")>0)
+            {
+                menuSelect--;
+                if (menuSelect<0)
+                {
+                    menuSelect = options.Length-1;
+                }
+            }
+            menuCool = 0;
+        }
+    }
+
+    bool MenuCooldown()
+    {
+        if (menuCool<maxMenuCool)
+        {
+            menuCool += Time.deltaTime;
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    void ChangeMenuColour()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            if (i == menuSelect)
+            {
+                options[i].color = Color.blue;
+            }
+            else
+            {
+                options[i].color = Color.white;
+            }
+        }
+    }
+
+    void ShowMenu()
+    {
+      foreach(Text t in options)
+            {
+                t.gameObject.SetActive(true);
+            }
+    }
+  // Update is called once per frame
+    void DisplayLeaderBoard()
+    {
+        if (!once)
+        {
+            List<KeyValuePair<string, int>> k = LeaderBoard.instance.ReturnLeaderBoard();
+            for (int i = 0; i < k.Count; i++)
+            {
+                rank[i].text = (i + 1).ToString();
+                scores[i].text = k[i].Value.ToString();
+                names[i].text = k[i].Key;
+                rank[i].color = Color.Lerp(Color.green, Color.red, (float)(i * 0.05f));
+                scores[i].color = Color.Lerp(Color.green, Color.red, (float)(i * 0.05f));
+                names[i].color = Color.Lerp(Color.green, Color.red, (float)(i * 0.05f));
+            }
+        }
+    }
+    void ShowLeaderBoard(bool _onOff)
+    {
+        if (_onOff)
+        {
+            foreach (GameObject g in titles)
+            {
+                g.SetActive(false);
+            }
+            lBoard.gameObject.SetActive(true);
+        }
+        else
+        {
+             lBoard.gameObject.SetActive(false);
+            
+            foreach (GameObject g in titles)
+            {
+                g.SetActive(true);
+            }
+        }
+    }
+    bool ButtonCoolDown()
+    {
+        if (buttonCool<maxButtonCool)
+        {
+            buttonCool += Time.deltaTime;
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 }
