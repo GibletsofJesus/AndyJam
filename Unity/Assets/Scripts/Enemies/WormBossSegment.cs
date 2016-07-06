eb684e71a4178ed4fa92f3f5ad9e4ddf6ce914f8
Shaun Landy <Shaun2.Landy@live.uwe.ac.uk>
2016-07-06 11:33:53 +0100

    WormBossStart

---
+++ Unity/Assets/Scripts/Enemies/WormBossSegment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WormBossSegment : Enemy
+{
+    [SerializeField] private WormBoss wormBoss = null;
+
+    protected override void Update()
+    {
+        if (GameStateManager.instance.state == GameStateManager.GameState.Gameplay)
+        {
+            Movement();
+        }
+    }
+
+    public override void TakeDamage(float _damage)
+    {
+        wormBoss.TakeDamage(_damage);
+    }
+
+    protected override void Movement()
+    {
+        
+    }
+}
